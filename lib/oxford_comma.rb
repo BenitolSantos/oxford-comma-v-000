664110d109407f9d80167d7ca855114eb7c1fee0
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-19 19:52:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,13 +3,9 @@ def oxford_comma(array)
     array[0]
   elsif array.size == 2
     array.join(" " + "and" + " ")
-  elsif array.size >= 3 
+  elsif array.size >= 3
       with_and = array[-1]
-      array.pop 
-  end 
-end     
-#      end
-#    else
-#    end
-#    comma_count.join("," + " ") + " and " + array[-1].to_s
-
+      array.pop
+      array.join(" " + "and" + " ") + " and " + with_and
+  end
+end
