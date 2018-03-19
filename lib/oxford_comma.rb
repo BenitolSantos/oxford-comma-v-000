59647c26c2bc30f3e43cb63a47328890e2ae39db
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-19 19:27:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,10 +5,13 @@ def oxford_comma(array)
     array.join(" " + "and" + " ")
   else
      array2 = Array.new
-     count = 1 
+     count = 1
     array.each do |element| until count == 2
       array2.push(element)
+      if count != 2
       count += 1
+      else
+      end
     end
   end
 end
