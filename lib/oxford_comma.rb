ba78bf5503c1fd3ddcc52b68be051a5baf66da0b
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-19 19:51:51 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,13 +3,13 @@ def oxford_comma(array)
     array[0]
   elsif array.size == 2
     array.join(" " + "and" + " ")
-  else
-     comma_count = Array.new
-     count = 1
-    array.each do |element| until count == (array.size - 1)
-      comma_count.push(element)
-      count += 1
-    end
-    comma_count.join("," + " ") + " and " + array[-1].to_s
-  end
-end
+  elsif array.size >= 3 
+      with_and = array[-1]
+      array.pop 
+  end 
+end     
+#      end
+#    else
+#    end
+#    comma_count.join("," + " ") + " and " + array[-1].to_s
+
